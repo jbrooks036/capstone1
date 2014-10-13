1b70196d7c55ab8d65c06018d92cd92ededa51ab
JoAnn Brooks <joannbrooks036@gmail.com>
2014-10-12 22:28:16 -0500

    collaborator views - in progress

--- client/views/projects/projects.js
+++ client/views/projects/projects.js
@@ -9,14 +9,14 @@
     $scope.project = {};
     $scope.projects = [];
     User.all().then(function(response){
-      console.log('client-controller >>>>>>>>>>>>>>>>>> User.all-response: ', response.data);
+      // console.log('client-controller >>>>>>>>>>>>>>>>>> User.all-response: ', response.data);
       $scope.users = response.data.users;
-      console.log('client-controller >>>>>>>>>>>>>>>>>> $scope.users: ', $scope.users);
+      // console.log('client-controller >>>>>>>>>>>>>>>>>> $scope.users: ', $scope.users);
     });
 
     // set up for Index of Projects
     Project.all().then(function(response){
-      console.log('client-controller-all >>>>>>>>>>>>response: ', response);
+      // console.log('client-controller-all >>>>>>>>>>>>response: ', response);
       $scope.projects = response.data.projects;
       console.log('client-controller-all >>>>>>>>>>>>$scope.projects: ', $scope.projects);
 
@@ -25,9 +25,21 @@
         // debugger;
 
         // replace researchers [array of user ids] with [array of user objects] -- to support display of login emails
-        console.log('client-controller-all >>>>>>>>>>>>$scope.projects[i]: ', $scope.projects[i]);
+        // console.log('client-controller-all >>>>>>>>>>>>$scope.projects[i]: ', $scope.projects[i]);
         for (var j=0; j < $scope.projects[i].researchers.length; j++) {
-          console.log('$scope.projects[i].researchers[j] -------- ', $scope.projects[i].researchers[j]);
+          // console.log('$scope.projects[i].researchers[j] -------- ', $scope.projects[i].researchers[j]);
+          if ($scope.projects[i].researchers[j].userId !== $scope.projects[i].currUserId){
+            var collabId = $scope.projects[i].researchers[j].userId;
+            for (var k=0; k < $scope.users.length; k++) {
+              if ($scope.users[k]._id === collabId) {
+
+                $scope.projects[i].collaborator = $scope.users[k];
+                console.log('$scope.projects[i].name: -------- ', $scope.projects[i].name);
+                console.log('$scope.projects[i].collaborator: -------- ', $scope.users[k]);
+              }
+            }
+          }
+          /*
           for (var k=0; k < $scope.users.length; k++) {
             console.log('$scope.users[k] initial: -------- ', $scope.users[k]);
             if ($scope.projects[i].researchers[j] === $scope.users[k]._id) {
@@ -35,20 +47,22 @@
               console.log('$scope.users[k] replaced: -------- ', $scope.users[k]);
             }
           }
+          */
         }
 
         // set up for Show Project
-        console.log('client-controller-all >>>>>>>>>>>>$routeParams: ', $routeParams);
+        // console.log('client-controller-all >>>>>>>>>>>>$routeParams: ', $routeParams);
         if ($routeParams.projectId) {
             if ($scope.projects[i]._id === $routeParams.projectId) {
                 $scope.project = $scope.projects[i];
+                console.log('setup for ShowProject, $scope.project: ', $scope.project);
                 break;
             }
           }
         }
 
         if ($routeParams.projectId) {
-          console.log('client-controller-all >>>>>>>>>>>>$routeParams2**: ', $routeParams);
+          // console.log('client-controller-all >>>>>>>>>>>>$routeParams2**: ', $routeParams);
           $location.path('/projects/' + $scope.project._id);
         }
 
@@ -84,10 +98,10 @@
 
     // Add New Project
     $scope.addProject = function(){
-      console.log('client-projects-controller-addProject >>>>>>>>>>$scope.project: ', $scope.project);
-      console.log('client-projects-controller-addProject >>>>>>>>>>$scope.files: ', $scope.files);
+      // console.log('client-projects-controller-addProject >>>>>>>>>>$scope.project: ', $scope.project);
+      // console.log('client-projects-controller-addProject >>>>>>>>>>$scope.files: ', $scope.files);
       Project.addProjectWithFiles($scope.project, $scope.files).then(function(response){
-      console.log('client-projects-controller-addProject >>>>>>>>>> response: ', response);
+      // console.log('client-projects-controller-addProject >>>>>>>>>> response: ', response);
         $scope.project = response.data.project;
         $scope.projects.push($scope.project);
         $scope.project = {};
@@ -99,6 +113,7 @@
       $scope.files = $files;
     };
 
+    /*
     // set up for Update Project (on Show page)
     $scope.toggleProject = function(){
       console.log('$scope.project>>>>>', $scope.project);
@@ -115,6 +130,7 @@
         // $window.location.reload();
       });
     };
+    */
 
     $scope.deleteProject = function(projectId){
       Project.deleteProject(projectId).then(function(response){
