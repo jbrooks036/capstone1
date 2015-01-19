1cf6325ecd28c1af6bf3c670412e89572e91509b
JoAnn Brooks <joannbrooks036@gmail.com>
2015-01-19 16:37:38 -0600

    fixed dependencies to inject angular-strap datepicker

--- client/index.js
+++ client/index.js
@@ -1,8 +1,9 @@
 (function(){
   'use strict';
 
-  angular.module('capstone1', ['ngRoute', 'LocalForageModule', 'angularFileUpload'])
+  angular.module('capstone1', ['ngRoute', 'LocalForageModule', 'angularFileUpload', 'mgcrea.ngStrap'])
   .config(['$routeProvider', '$httpProvider', '$localForageProvider', function($routeProvider, $httpProvider, $localForageProvider){
+  // .config(['$routeProvider', '$httpProvider', '$localForageProvider', function($routeProvider, $httpProvider, $localForageProvider){
     $routeProvider
     .when('/', {templateUrl:'/views/home/home.html', controller:'HomeCtrl'})
     .when('/about', {templateUrl:'/views/about/about.html', controller:'AboutCtrl'})
